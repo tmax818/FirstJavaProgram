c167deffb965579acea9bdc2dbda74fd1d56936a
tmax818 <tylermaxwell661@gmail.com>
2022-12-15 08:55:04 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //! TODO add method that prints string to console
+        // TODO add method that prints string to console
     }
 }
